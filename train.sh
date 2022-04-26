8ce9f6d1a28f65a9533d574c39639a9cc4e95d57
rasd3 <yckim@spa.hanyang.ac.kr>
2022-04-26 08:04:00 +0000

    [add] hybrid optimizer tuning congit

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
-bash ./tools/dist_train.sh ./configs/mvxnet/nuscenes_actr/dv_mvx-fpn_center_img_secfpn_adamw_2x8_80e_actr_nus-3d-3class.py 4 --work-dir ./work_dirs/mvxnet_nus/actr_dv_2x4_1e-5
-bash ./tools/dist_train.sh ./configs/mvxnet/nuscenes_actr/dv_mvx-fpn_center_img_secfpn_adamw_2x8_80e_actr_ne2_nus-3d-3class.py 4 --work-dir ./work_dirs/mvxnet_nus/actr_dv_2x4_ne2_3e-5
+bash ./tools/dist_train.sh ./configs/mvxnet/nuscenes_actr/dv_mvx-fpn_center_img_secfpn_adamw_2x8_80e_actr_ne2_nus_cbgs-3d-3class_hybrid6.py 4 --work-dir ./work_dirs/mvxnet_nus/actr_dv_2x4_hybrid6 --deterministic
+bash ./tools/dist_train.sh ./configs/mvxnet/nuscenes_actr/dv_mvx-fpn_center_img_secfpn_adamw_2x8_80e_actr_ne2_nus_cbgs-3d-3class_hybrid7.py 4 --work-dir ./work_dirs/mvxnet_nus/actr_dv_2x4_hybrid7 --deterministic
